550c8aee73248f5c84735556ab547ad7c5455d62
mohammadyazarloo2 <34187298+mohammadyazarloo2@users.noreply.github.com>
2020-12-01 19:10:16 +0330

    set up backend

---
+++ backend/data.js
@@ -0,0 +1,78 @@
+const data={
+    products:[
+        {
+            _id:'1',
+            name:'nike slim shirt',
+            category:'shirt',
+            image:'../images/p1.jpg',
+            price:120,
+            countInStock:10,
+            brand:'nike',
+            rating:4.5,
+            numReviews:10,
+            description:'nike brand shirt product'
+        },
+        {
+            _id:'2',
+            name:'adidas slim shirt',
+            category:'shirt',
+            image:'../images/p2.jpg',
+            price:100,
+            countInStock:20,
+            brand:'adidas',
+            rating:4.0,
+            numReviews:10,
+            description:'adidas brand shirt product'
+        },
+        {
+            _id:'3',
+            name:'locoste slim shirt',
+            category:'shirt',
+            image:'../images/p3.jpg',
+            price:230,
+            countInStock:0,
+            brand:'locoste',
+            rating:4.8,
+            numReviews:17,
+            description:'locoste brand shirt product'
+        },
+        {
+            _id:'4',
+            name:'nike slim pant',
+            category:'pants',
+            image:'../images/p4.jpg',
+            price:78,
+            countInStock:15,
+            brand:'nike',
+            rating:4.0,
+            numReviews:10,
+            description:'nike brand pant product'
+        },
+        {
+            _id:'5',
+            name:'puma slim pant',
+            category:'pants',
+            image:'../images/p5.jpg',
+            price:65,
+            countInStock:5,
+            brand:'puma',
+            rating:4.5,
+            numReviews:10,
+            description:'puma brand pant product'
+        },
+        {
+            _id:'6',
+            name:'adidas slim pant',
+            category:'pants',
+            image:'../images/p6.jpg',
+            price:139,
+            countInStock:12,
+            brand:'adidas',
+            rating:4.5,
+            numReviews:15,
+            description:'adidas brand shirt product'
+        },
+    ]
+}
+
+export default data
